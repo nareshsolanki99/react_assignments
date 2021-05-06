fb82463face5e79554bb8571844ea3ad44439400
naresh solanki <nareshsolanki3456@gmail.com>
2021-05-07 01:38:47 +0530

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my First React App</h1>
+      <h2>Lets Explore React</h2>
+    </div>
+  );
+}
+
+export default App;
